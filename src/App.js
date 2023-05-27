e58ca15e71778b428fd43f9f185da142ed65bfd8
Abraham <abraham.hdz.v14@gmail.com>
2023-05-26 19:08:32 -0600

    primer proyecto react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+/*import Card from './Components/Card';*/
+import Cards from './Components/Cards';
+
+
+function App() {
+    return ( <div className = "App">
+        Hello World!!
+        <Cards></Cards>
+        </div>
+    );
+}
+
+export default App;
